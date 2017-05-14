5a99f2f4baf9cc8bb89160e5887244edb64380b6
grhjkim <hyunjinkim0411@gmail.com>
2017-05-14 23:13:48 +0900

    Update main.c

--- gugudan/main.c
+++ gugudan/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-
+void printGugudan(void)
+{
+    int dan;
+    int multiplier;
+    int value;
+    for(dan = 2; dan<=9; dan++) {
+        for(multiplier = 1; multiplier<=9; multiplier++) {
+            value = dan * multiplier;
+            printf("%d * %d = %02d\n", dan, multiplier, value);
+        }
+        printf("\n");
+    }
+}
 int main()
 {
+    printGugudan();
+
     return 0;
 }
-
